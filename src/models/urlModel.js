0cfb4ff6ec173dcb4127b9dfb531cce4fb027d32
sabiha <you@example.com>
2022-09-29 16:45:43 +0530

    .

--- src/models/urlModel.js
+++ src/models/urlModel.js
@@ -1,9 +1,23 @@
-const mongoose= require('mongoose')
+const mongoose = require("mongoose");
 
-const urlSchema=new mongoose.Schema({
-     urlCode: { type:String,required:true, unique:true, lowercase:true, trim:true }, 
-    longUrl: {type:String,required:true},
-     shortUrl: {type:String,required:true, unique:true} 
-    })
+const urlSchema = new mongoose.Schema({
+  urlCode: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  longUrl: {
+    type: String,
+    required: true,
+  },
 
-module.exports=mongoose.model('URL',urlSchema);
+  shortUrl: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+module.exports = mongoose.model("URL", urlSchema);
