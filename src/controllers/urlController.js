5350628674971e7889df664076c0ea0b4cf950c9
Rahul-Singh-Dhek <rahuldhek21@gmail.com>
2022-10-03 22:15:02 +0530

    RD

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -2,12 +2,30 @@ const mongoose = require('mongoose')
 const shortId = require('shortid')
 // const validURL = require('valid-url')
 const urlModel = require('../models/urlModel')
-// console.log(shortId)
+const redis = require('redis')
+const { promisify } = require("util");
+
+
+const redisClient = redis.createClient(
+    13960,
+    "redis-13960.c276.us-east-1-2.ec2.cloud.redislabs.com",
+    { no_ready_check: true }
+);
+
+redisClient.auth("NQpIjO7ZCxuRi3APtBd1ppZmfFbFTmQP", function (err) {
+    if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+    console.log("Connected to Redis..");
+});
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 
 const shorturl = async function (req, res) {
-    try {
-
+    // try {
         let longUrl = req.body.longUrl
         if (!longUrl) {
             return res.status(400).send({ status: false, message: "Please provide longUrl" })
@@ -18,27 +36,31 @@ const shorturl = async function (req, res) {
         longUrl = longUrl.trim().toLowerCase()
 
         let gau = longUrl.startsWith("http://") || longUrl.startsWith("https://") || longUrl.startsWith("ftp://")
-        // let regForUrl = /(:?^((https|http|HTTP|HTTPS){1}:\/\/)(([w]{3})[\.]{1})?([a-zA-Z0-9]{1,}[\.])[\w]((\/){1}([\w@? ^=%&amp;~+#-_.]+)))$/
         if (!gau) {
             return res.status(400).send({ status: false, message: "Please provide valid LongUrl" })
         }
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        let catchedUrldata = await GET_ASYNC(`${longUrl}`)
 
-        // if (regForUrl.test(longUrl)) {
-        //     return res.status(400).send({ status: false, message: "Rgex:Please provide a valid URL" })
-        // }
+        if (catchedUrldata) {
+            return res.status(200).send({ status: true, message: "ShortUrl is already created for this URL(REDIS)", data: JSON.parse(catchedUrldata) })
+        }
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+       
 
-        // if (!validURL.isUri(longUrl)) {
-        //     return res.status(400).send({ status: false, message: "Please provide a valid URL" })
-        // }
         let url = await urlModel.findOne({ longUrl: longUrl }).select({ _id: 0, __v: 0 })
         if (url) {
+            await SET_ASYNC(`${longUrl}`,JSON.stringify(url))//stringify converts objects into string
+            //Redis consider only string
             return res.status(200).send({ status: true, message: "ShortUrl is already created for this URL", data: url })
         }
 
         let urlCode = shortId.generate();
         let baseUrl = "http://localhost:3000/"
         let shortUrl = baseUrl + urlCode;
+
         let savedData = await urlModel.create({ urlCode: urlCode, longUrl: longUrl, shortUrl: shortUrl })
+
         return res.status(201).send({
             status: true, message: "ShortUrl Generated Successfully", data: {
                 urlCode: savedData.urlCode,
@@ -46,19 +68,30 @@ const shorturl = async function (req, res) {
                 shortUrl: savedData.shortUrl
             }
         })
-    }
-    catch (err) {
-        return res.status(200).send({ status: true, message: "ShortUrl is already created for this URL", data: url })
-    }
+    // }
+    // catch (err) {
+    //     return res.status(500).send({ status: true, message: err })
+    // }
 }
 
 const geturl = async function (req, res) {
-    // console.log(typeof req.params.urlCode)
     if (!shortId.isValid(req.params.urlCode.trim())) {
         return res.status(400).send({ status: false, message: "Please provide valid urlCode" })
     }
-    const url = await urlModel.findOne({ urlCode: req.params.urlCode })
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    let catchedUrldata = await GET_ASYNC(`${req.params.urlCode.trim()}`)
+    // console.log(catchedUrldata)
+    if (catchedUrldata) {
+        console.log("I am in")
+        return res.status(302).redirect(JSON.parse(catchedUrldata).longUrl)
+    }
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    const url = await urlModel.findOne({ urlCode: req.params.urlCode.trim() })
     if (url) {
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        await SET_ASYNC(`${req.params.urlCode.trim()}`, JSON.stringify(url))
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         return res.status(302).redirect(url.longUrl)
     } else {
         return res.status(400).send({ status: false, message: "No documnet found with this urlCode" });
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const route = require("./routes/route.js");
 const mongoose = require("mongoose");
 const app = express();
 
+
 app.use(express.json());
 
 mongoose.connect("mongodb+srv://RahulSinghDhek:18248518@cluster0.dxzlfnc.mongodb.net/group2Database?retryWrites=true&w=majority",
@@ -18,8 +19,13 @@ mongoose.connect("mongodb+srv://RahulSinghDhek:18248518@cluster0.dxzlfnc.mongodb
   }
 )
 
-app.use("/", route);
+app.use("/", route); 
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("Express app running on port " + (process.env.PORT || 3000));
 });
+
+
+// console.log(util);
+//Connect to redis
+
